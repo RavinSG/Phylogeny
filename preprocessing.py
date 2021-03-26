12478b70c061bc9318cfebab7a5b840905e04afe
ravin <ravinsg@gmail.com>
2021-03-26 21:19:59 +0530

    Extract relevant gene from the genome.

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,11 @@
+import os
 import json
 from collections import defaultdict
+
 import numpy as np
 import pandas as pd
 from Bio import Entrez, SeqIO
+from Bio.Seq import Seq
 
 Entrez.email = "test@gmail.com"
 
@@ -15,9 +18,14 @@ file = pd.ExcelFile("Supporting_Documents/protein_tables.xlsx")
 sheets = file.sheet_names
 num_proteins = len(proteins)
 
+if os.path.exists("genomes.json"):
+    print("Genome dictionary found, loading data...")
+    with open("genomes.json") as gf:
+        genome_dict = json.load(gf)
+
 
 def extract_protein_info(protein_sheets):
-    protein_database = defaultdict(list)
+    protein_database = defaultdict(dict)
     protein_profiles = []
     for sheet in protein_sheets:
         df = file.parse(sheet)
@@ -29,8 +37,8 @@ def extract_protein_info(protein_sheets):
                 one_hot[i] = 1
                 sheet_index = np.where(sheet_proteins == proteins[i])[0][0]
                 protein_row = df.iloc[sheet_index]
-                protein_data = [sheet, protein_row['Start'], protein_row['Stop'], protein_row['Strand']]
-                protein_database[proteins[i]].append(protein_data)
+                protein_data = [protein_row['Start'], protein_row['Stop'], protein_row['Strand']]
+                protein_database[proteins[i]][sheet] = protein_data
 
         protein_profiles.append(one_hot)
 
@@ -47,7 +55,7 @@ def extract_protein_info(protein_sheets):
 
 
 def download_genome(species_id):
-    print(f"Downloading {species_id}...")
+    print(f"Downloading {species_id}")
     handle = Entrez.efetch(db="nucleotide", id=species_id, rettype="fasta", retmode="text")
     record = SeqIO.read(handle, "fasta")
     return record.seq
@@ -62,6 +70,18 @@ def generate_gene_dict(species):
     with open("genomes.json", 'w') as gf:
         json.dump(gene_dict, gf)
 
+    return gene_dict
+
 
 # generate_gene_dict(sheets)
-a,b = extract_protein_info(sheets)
+p_database, _ = extract_protein_info(sheets)
+
+extracted_sequences = defaultdict(dict)
+for genome in sheets:
+    genome_seq = Seq(genome_dict[genome.strip()])
+    for protein in proteins:
+        protein_data = p_database[protein][genome]
+        if protein_data[-1] == '+':
+            extracted_sequences[protein][genome] = (genome_seq[protein_data[0]:protein_data[1]])
+        else:
+            extracted_sequences[protein][genome] = (genome_seq[protein_data[0]:protein_data[1]])

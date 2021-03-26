865d0c4dc88988e2a280f0110e698d23438338ce
ravin <ravinsg@gmail.com>
2021-03-26 20:07:12 +0530

    Extract information relating to the protein location from excel sheet.

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import json
-import pandas as pd
+from collections import defaultdict
 import numpy as np
+import pandas as pd
 from Bio import Entrez, SeqIO
 
 Entrez.email = "test@gmail.com"
@@ -8,31 +9,41 @@ Entrez.email = "test@gmail.com"
 proteins = ["single-stranded DNA-binding protein", "LysR family transcriptional regulator",
             "helix-turn-helix domain-containing protein", "efflux transporter outer membrane subunit"]
 
+# proteins = ["FUSC family protein", "MFS transporter", "YraN family protein"]
+
 file = pd.ExcelFile("Supporting_Documents/protein_tables.xlsx")
 sheets = file.sheet_names
-protein_profiles = []
+num_proteins = len(proteins)
 
-for sheet in sheets:
-    df = file.parse(sheet)
-    num_proteins = len(proteins)
-    one_hot = [0] * num_proteins
 
-    sheet_proteins = df['Protein name'].values
+def extract_protein_info(protein_sheets):
+    protein_database = defaultdict(list)
+    protein_profiles = []
+    for sheet in protein_sheets:
+        df = file.parse(sheet)
+        one_hot = [0] * num_proteins
+        sheet_proteins = df['Protein name'].apply(lambda x: x.split(":")[-1].strip()).values
 
-    for i in range(num_proteins):
-        if proteins[i] in sheet_proteins:
-            one_hot[i] = 1
+        for i in range(num_proteins):
+            if proteins[i] in sheet_proteins:
+                one_hot[i] = 1
+                sheet_index = np.where(sheet_proteins == proteins[i])[0][0]
+                protein_row = df.iloc[sheet_index]
+                protein_data = [sheet, protein_row['Start'], protein_row['Stop'], protein_row['Strand']]
+                protein_database[proteins[i]].append(protein_data)
 
-    protein_profiles.append(one_hot)
+        protein_profiles.append(one_hot)
 
-protein_profiles = np.array(protein_profiles)
-protein_count = np.sum(protein_profiles, axis=0)
+    protein_profiles = np.array(protein_profiles)
+    protein_count = np.sum(protein_profiles, axis=0)
 
-final_proteins = []
-for count, protein in zip(protein_count, proteins):
-    if count != len(sheets):
-        continue
-    final_proteins.append(protein)
+    final_proteins = []
+    for count, protein in zip(protein_count, proteins):
+        if count != len(protein_sheets):
+            continue
+        final_proteins.append(protein)
+
+    return protein_database, protein_profiles
 
 
 def download_genome(species_id):
@@ -52,4 +63,5 @@ def generate_gene_dict(species):
         json.dump(gene_dict, gf)
 
 
-generate_gene_dict(sheets)
+# generate_gene_dict(sheets)
+a,b = extract_protein_info(sheets)

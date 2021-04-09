2d967746009a52695d6da7e9aa9dff5405a9c559
ravin <ravinsg@gmail.com>
2021-04-09 21:41:42 +0530

    Code cleanup.

--- preprocessing.py
+++ preprocessing.py
@@ -6,9 +6,11 @@ import numpy as np
 import pandas as pd
 from Bio import Entrez, SeqIO
 from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 Entrez.email = "test@gmail.com"
 
+# List of proteins to analyze
 proteins = ["single-stranded DNA-binding protein", "LysR family transcriptional regulator",
             "helix-turn-helix domain-containing protein", "efflux transporter outer membrane subunit"]
 
@@ -18,12 +20,8 @@ file = pd.ExcelFile("Supporting_Documents/protein_tables.xlsx")
 sheets = file.sheet_names
 num_proteins = len(proteins)
 
-if os.path.exists("genomes.json"):
-    print("Genome dictionary found, loading data...")
-    with open("genomes.json") as gf:
-        genome_dict = json.load(gf)
 
-
+# Extract the starting and ending location of each protein for the genome given
 def extract_protein_info(protein_sheets):
     protein_database = defaultdict(dict)
     protein_profiles = []
@@ -37,8 +35,8 @@ def extract_protein_info(protein_sheets):
                 one_hot[i] = 1
                 sheet_index = np.where(sheet_proteins == proteins[i])[0][0]
                 protein_row = df.iloc[sheet_index]
-                protein_data = [protein_row['Start'], protein_row['Stop'], protein_row['Strand']]
-                protein_database[proteins[i]][sheet] = protein_data
+                protein_database[proteins[i]][sheet] = \
+                    [protein_row['Start'], protein_row['Stop'], protein_row['Strand']]
 
         protein_profiles.append(one_hot)
 
@@ -46,14 +44,15 @@ def extract_protein_info(protein_sheets):
     protein_count = np.sum(protein_profiles, axis=0)
 
     final_proteins = []
-    for count, protein in zip(protein_count, proteins):
+    for count, f_protein in zip(protein_count, proteins):
         if count != len(protein_sheets):
             continue
-        final_proteins.append(protein)
+        final_proteins.append(f_protein)
 
     return protein_database, protein_profiles
 
 
+# If not available, download the genome sequences from www.ncbi.nlm.nih.gov
 def download_genome(species_id):
     print(f"Downloading {species_id}")
     handle = Entrez.efetch(db="nucleotide", id=species_id, rettype="fasta", retmode="text")
@@ -61,27 +60,47 @@ def download_genome(species_id):
     return record.seq
 
 
+# Save the downloaded genomes to a json file for future uses
 def generate_gene_dict(species):
     gene_dict = {}
     for gene in species:
         gene = gene.strip()
         gene_dict[gene] = download_genome(gene)._data
 
-    with open("genomes.json", 'w') as gf:
-        json.dump(gene_dict, gf)
+    with open("genomes.json", 'w') as g_file:
+        json.dump(gene_dict, g_file)
 
     return gene_dict
 
 
-# generate_gene_dict(sheets)
+def write_to_fasta(protein_name, sequences, out_file):
+    SeqIO.write(sequences[protein_name].values(), out_file, "fasta")
+
+
+# If the genome dictionary is not available, download them from ncbi
+if os.path.exists("genomes.json"):
+    print("Genome dictionary found, loading data...")
+    with open("genomes.json") as gf:
+        genome_dict = json.load(gf)
+else:
+    print("Genome dictionary not found, downloading data...")
+    genome_dict = generate_gene_dict(sheets)
+
 p_database, _ = extract_protein_info(sheets)
 
+# Add all the sequences relating to a protein to a seperate dictionary
 extracted_sequences = defaultdict(dict)
 for genome in sheets:
     genome_seq = Seq(genome_dict[genome.strip()])
     for protein in proteins:
         protein_data = p_database[protein][genome]
         if protein_data[-1] == '+':
-            extracted_sequences[protein][genome] = (genome_seq[protein_data[0]:protein_data[1]])
+            extracted_sequences[protein][genome] = SeqRecord(
+                genome_seq[protein_data[0]:protein_data[1]], id=genome, description=protein)
         else:
-            extracted_sequences[protein][genome] = (genome_seq[protein_data[0]:protein_data[1]])
+            extracted_sequences[protein][genome] = SeqRecord(
+                genome_seq[protein_data[0]:protein_data[1]].reverse_complement(), id=genome, description=protein)
+
+# Save each protein in a separate file
+for protein in proteins:
+    write_to_fasta(protein, extracted_sequences, f"{protein}_sequences.fasta")
--- robinson_foulds.py
+++ robinson_foulds.py
@@ -3,7 +3,7 @@ import copy
 from collections import Counter
 
 
-#get leaf names of a tree (bipartition), for rooted trees,  add root as additional leaf to count clades
+# get leaf names of a tree (bipartition), for rooted trees,  add root as additional leaf to count clades
 def get_leaves(nd, root=None):
     leaves = []
     for l in nd:
@@ -13,66 +13,66 @@ def get_leaves(nd, root=None):
     return leaves
 
 
-#compare two unordered lists
+# compare two unordered lists
 def compare(s, t):
     return Counter(s) == Counter(t)
 
 
-#get leafsets of all bipartitions of an unrooted tree
+# get leafsets of all bipartitions of an unrooted tree
 def get_leafset(t1):
     count2 = 0
     count3 = 0
     t1_leafsets = []
 
     for node in t1.traverse("levelorder"):
-        count2 +=1
+        count2 += 1
 
-        #skip leaves
-        if len(node.children) ==0:
+        # skip leaves
+        if len(node.children) == 0:
             continue
 
-        #rename children with no name (to keep track)
+        # rename children with no name (to keep track)
         for child in node.children:
             if child.name == "":
-                count3 +=1
+                count3 += 1
                 child.name = str(count3)
 
-        #append partitions of both children
+        # append partitions of both children
         t1_leafsets.append(get_leaves(node.children[0]))
         t1_leafsets.append(get_leaves(node.children[1]))
 
-        #skip root (only for unrooted trees)
+        # skip root (only for unrooted trees)
         # if count2 ==1:
         #     continue
 
         tree_cpy1 = copy.deepcopy(t1)
         tree_cpy2 = copy.deepcopy(t1)
-        #print("name", node.children[0].name)
+        # print("name", node.children[0].name)
 
-        #break tree into two removing a child node
+        # break tree into two removing a child node
         ch1 = tree_cpy1.search_nodes(name=node.children[0].name)[0]
         ch2 = tree_cpy2.search_nodes(name=node.children[1].name)[0]
         ch1.detach()
         ch2.detach()
-        #tree_cpy1 = tree_cpy1.remove_child(ch1)
-        #tree_cpy2 = tree_cpy1.remove_child(ch2)
+        # tree_cpy1 = tree_cpy1.remove_child(ch1)
+        # tree_cpy2 = tree_cpy1.remove_child(ch2)
         t1_leafsets.append(get_leaves(tree_cpy1, "r"))
         t1_leafsets.append(get_leaves(tree_cpy2, "r"))
 
-    #t1.show()
-    return (t1_leafsets)
+    # t1.show()
+    return t1_leafsets
     # for leaf in t1:
     #     print(len(leaf.get_children()))
 
 
-#get robinson foulds distance of one tree in comparison to the other
+# get robinson foulds distance of one tree in comparison to the other
 def robinson_foulds_one_way(lf1, lf2):
     c = 0
     for i in lf1:
-        #print(len(i))
+        # print(len(i))
         found = False
         for j in lf2:
-            if compare(i,j):
+            if compare(i, j):
                 found = True
                 break
         if not found:
@@ -80,18 +80,18 @@ def robinson_foulds_one_way(lf1, lf2):
     return c
 
 
-#get robinsond foulds distance between 2 trees
+# get Robinson-Foulds distance between 2 trees
 def robinson_foulds_dist(lf1, lf2):
     c1 = robinson_foulds_one_way(lf1, lf2)
     c2 = robinson_foulds_one_way(lf2, lf1)
-    return ((c1 + c2)/2)
+    return ((c1 + c2) / 2)
 
 
-#compare a set of trees
+# compare a set of trees
 def compare_trees(trees):
-    for i,tree in enumerate(trees):
-        for j,tree2 in enumerate(trees):
-            if (i == j):
+    for i, tree in enumerate(trees):
+        for j, tree2 in enumerate(trees):
+            if i == j:
                 continue
             lf1 = get_leafset(tree)
             lf2 = get_leafset(tree2)
@@ -100,17 +100,15 @@ def compare_trees(trees):
 
 
 # Load a tree structure from a newick file.
-t0 = Tree("LysR family transcriptional regulator_sequences.nwk")
-t1 = Tree("single-stranded DNA-binding protein.nwk")
-t2 = Tree("efflux transporter outer membrane subunit_sequences.nwk")
-t3 = Tree("helix-turn-helix domain-containing protein_sequences.nwk")
+t0 = Tree("Trees/LysR family transcriptional regulator_sequences.nwk")
+t1 = Tree("Trees/single-stranded DNA-binding protein.nwk")
+t2 = Tree("Trees/efflux transporter outer membrane subunit_sequences.nwk")
+t3 = Tree("Trees/helix-turn-helix domain-containing protein_sequences.nwk")
 
-
-#remove protein name
+# remove protein name
 for tree in [t0, t1, t2, t3]:
     for leaf in tree:
         leaf.name = leaf.name.split("_")[0] + "_" + leaf.name.split("_")[1]
-        #print (leaf.name)
+        # print (leaf.name)
 
 compare_trees([t0, t1, t2, t3])
-
